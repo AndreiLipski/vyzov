ea7ee9b0d39347f645ef2f7297e03f48bfb94d86
Andrei L <davinchi.l.a.s@mail.ru>
2022-12-11 15:38:32 +0300

    12

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./js/common.js";
+import "./assets/css/main.css";
+import "./assets/scss/main.scss";
+
+
+import './js/burger.js';
+// import './js/swiper.js';
+import './js/scrol.js';
+import './js/program.js';
+import './js/gallery-collapse.js';
+//
+// if (process.env.NODE_ENV !== `production`) {
+//   async function load() {
+//     let resize_info = await import("./smart-gread-layer/smart-gread-layer");
+//     resize_info.default();
+//   }
+//
+//   load();
+// }
